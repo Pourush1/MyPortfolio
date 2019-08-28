a55875a511e5b9c20e8b491dce07eaeced577757
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2019-08-28 11:56:45 -0500

    Updates

---
+++ precache-manifest.baa43ada4eba163bec75e2d57e87d43e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933bab1f94e2eb039af214763b0671e4",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "a48b0559325d21cb19ea",
+    "url": "/MyPortfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "86273232b9a94d9ea5b6",
+    "url": "/MyPortfolio/static/js/2.23af08e9.chunk.js"
+  },
+  {
+    "revision": "a48b0559325d21cb19ea",
+    "url": "/MyPortfolio/static/js/main.3d35e6b4.chunk.js"
+  },
+  {
+    "revision": "61aec93950ce5316de78",
+    "url": "/MyPortfolio/static/js/runtime~main.c109451e.js"
+  }
+]);
