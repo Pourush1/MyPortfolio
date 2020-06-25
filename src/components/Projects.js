d225dd26614ebaeeb377e42e83e7d37010658769
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-06-24 22:35:10 -0700

    changed some styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,159 +24,6 @@ class Projects extends Component {
               >
                 <div
                   className="project"
-                  style={{ backgroundImage: "url(images/slotGameImage.JPG)" }}
-                >
-                  <div className="desc">
-                    <div className="con">
-                      <h1>Slot Game Machine</h1>
-                      <p>
-                        <a
-                          className="btn btn-primary btn-learn"
-                          href="https://github.com/Pourush1/SlotGameWithImages"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <h3>View Code</h3> <i className="icon-github" />
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="col-md-4 animate-box"
-                data-animate-effect="fadeInRight"
-              >
-                <div
-                  className="project"
-                  style={{ backgroundImage: "url(images/roboSearch.JPG)" }}
-                >
-                  <div className="desc">
-                    <div className="con">
-                      <h1>Robo Friends</h1>
-                      <p>
-                        <a
-                          className="btn btn-primary btn-learn"
-                          href="https://github.com/Pourush1/RoboApp"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <h3>View Code</h3> <i className="icon-github" />
-                        </a>
-                      </p>
-                      <p>
-                        <a
-                          className="btn btn-primary btn-learn"
-                          href="https://robo-search-app.herokuapp.com/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <h3>View Live Project</h3>{" "}
-                          <i className="icon-github" />
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="col-md-4 animate-box"
-                data-animate-effect="fadeInTop"
-              >
-                <div
-                  className="project"
-                  style={{ backgroundImage: "url(images/colorGame.JPG)" }}
-                >
-                  <div className="desc">
-                    <div className="con">
-                      <h1>Color Game</h1>
-                      <p>
-                        <a
-                          className="btn btn-primary btn-learn"
-                          href="https://github.com/Pourush1/ColorGame"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <h3>View Code</h3> <i className="icon-github" />
-                        </a>
-                      </p>
-                      {/* <h3>
-                        <a href="work.html">Work 03</a>
-                      </h3>
-                      <span>Illustration</span>
-                      <p className="icon">
-                        <span>
-                          <a href="#">
-                            <i className="icon-share3" />
-                          </a>
-                        </span>
-                        <span>
-                          <a href="#">
-                            <i className="icon-eye" /> 100
-                          </a>
-                        </span>
-                        <span>
-                          <a href="#">
-                            <i className="icon-heart" /> 49
-                          </a>
-                        </span>
-                      </p> */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="col-md-4 animate-box"
-                data-animate-effect="fadeInBottom"
-              >
-                <div
-                  className="project"
-                  style={{ backgroundImage: "url(images/jumble.JPG)" }}
-                >
-                  <div className="desc">
-                    <div className="con">
-                      {/* <h3>
-                        <a href="work.html">Work 04</a>
-                      </h3>
-                      <span>Application</span>
-                      <p className="icon">
-                        <span>
-                          <a href="#">
-                            <i className="icon-share3" />
-                          </a>
-                        </span>
-                        <span>
-                          <a href="#">
-                            <i className="icon-eye" /> 100
-                          </a>
-                        </span>
-                        <span>
-                          <a href="#">
-                            <i className="icon-heart" /> 49
-                          </a>
-                        </span>
-                      </p> */}
-                      <h1>Jumble Game</h1>
-                      <p>
-                        <a
-                          className="btn btn-primary btn-learn"
-                          href="https://github.com/Pourush1/Jumble"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <h3>View Code</h3> <i className="icon-github" />
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="col-md-4 animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <div
-                  className="project"
                   style={{ backgroundImage: "url(images/covid-19.JPG)" }}
                 >
                   <div className="desc">
@@ -237,6 +84,159 @@ class Projects extends Component {
                         >
                           <h3>View Live Project</h3>{" "}
                           <i className="icon-github" />
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInTop"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/roboSearch.JPG)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h1>Robo Friends</h1>
+                      <p>
+                        <a
+                          className="btn btn-primary btn-learn"
+                          href="https://github.com/Pourush1/RoboApp"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <h3>View Code</h3> <i className="icon-github" />
+                        </a>
+                      </p>
+                      <p>
+                        <a
+                          className="btn btn-primary btn-learn"
+                          href="https://robo-search-app.herokuapp.com/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <h3>View Live Project</h3>{" "}
+                          <i className="icon-github" />
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/colorGame.JPG)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h1>Color Game</h1>
+                      <p>
+                        <a
+                          className="btn btn-primary btn-learn"
+                          href="https://github.com/Pourush1/ColorGame"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <h3>View Code</h3> <i className="icon-github" />
+                        </a>
+                      </p>
+                      {/* <h3>
+                        <a href="work.html">Work 03</a>
+                      </h3>
+                      <span>Illustration</span>
+                      <p className="icon">
+                        <span>
+                          <a href="#">
+                            <i className="icon-share3" />
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-eye" /> 100
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-heart" /> 49
+                          </a>
+                        </span>
+                      </p> */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInBottom"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/slotGameImage.JPG)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      <h1>Slot Game Machine</h1>
+                      <p>
+                        <a
+                          className="btn btn-primary btn-learn"
+                          href="https://github.com/Pourush1/SlotGameWithImages"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <h3>View Code</h3> <i className="icon-github" />
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="col-md-4 animate-box"
+                data-animate-effect="fadeInRight"
+              >
+                <div
+                  className="project"
+                  style={{ backgroundImage: "url(images/jumble.JPG)" }}
+                >
+                  <div className="desc">
+                    <div className="con">
+                      {/* <h3>
+                        <a href="work.html">Work 04</a>
+                      </h3>
+                      <span>Application</span>
+                      <p className="icon">
+                        <span>
+                          <a href="#">
+                            <i className="icon-share3" />
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-eye" /> 100
+                          </a>
+                        </span>
+                        <span>
+                          <a href="#">
+                            <i className="icon-heart" /> 49
+                          </a>
+                        </span>
+                      </p> */}
+                      <h1>Jumble Game</h1>
+                      <p>
+                        <a
+                          className="btn btn-primary btn-learn"
+                          href="https://github.com/Pourush1/Jumble"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <h3>View Code</h3> <i className="icon-github" />
                         </a>
                       </p>
                     </div>
